e649a789a51a6a7340089ed6af5f4d9075510740
Erik Hoelzel <erik.hoelzel@posteo.de>
2023-01-12 14:20:06 +0100

    iban exercise

---
+++ de/brightslearning/java/basics/IBANValidator.java
@@ -0,0 +1,12 @@
+package de.brightslearning.java.basics;
+
+/**
+ * Validate a given String if it represents a German IBAN. Following criteria have to fulfilled:
+ * <ul>
+ *     <li>String starting with "DE"</li>
+ *     <li>Length: 22</li>
+ *     <li>Modulo 97 validation rule fulfilled (see: Wikipedia)</li>
+ * </ul>
+ * @link https://de.wikipedia.org/wiki/Internationale_Bankkontonummer#Prüfsumme
+ */public class IBANValidator {
+}
