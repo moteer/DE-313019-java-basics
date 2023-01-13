68399e5d1020fb834cb8033f044c73e0a62f7cd3
Erik Hoelzel <erik.hoelzel@posteo.de>
2023-01-13 10:24:38 +0100

    iban exercise

--- de/brightslearning/java/basics/methods/Methods.java
+++ de/brightslearning/java/basics/methods/Methods.java
@@ -2,37 +2,41 @@ package de.brightslearning.java.basics.methods;
 
 public class Methods {
 
-    int instanzVariable;
 
+    /**
+     * Sichbarkeiten:
+     * - public    : sichtbar für jeden
+     * - private   : sichtbar nur für die Klasse in der sie deklariert ist
+     * - protected : im selben Package sichtbar und als sichtbar für erbende Objecte
+     * - "nichts"  : nur im gleichen package sichtbar
+     */
     public static final String plus(Params param) {
         return "";
+    }
+
+    public void myPublicMethod() {
+        //  - public    : sichtbar für jeden
     }
 
     private void myPrivateMethod() {
         myProtectedMethod();
         myPackagePrivateMethod();
+        //  - private   : sichtbar nur für die Klasse in der sie deklariert ist
     }
 
     protected void myProtectedMethod() {
-
+        //  - protected : im selben Package sichtbar und als sichtbar für erbende Objecte
     }
 
     void myPackagePrivateMethod() {
-
+        //  - "nichts"  : nur im gleichen package sichtbar
     }
 
-    // Sichbarkeit:
-    //  - public    : sichtbar für jeden
-    //  - private   : sichtbar nur für die Klasse in der sie deklariert ist
-    //  - protected : im selben Package sichtbar und als sichtbar für erbende Objecte
-    //  - "nichts"  : nur im gleichen package sichtbar
-
-
-    //static        : Instanz/Object-unabhängige Methode, Es muss kein Object instanziiert werden
-
-
+    /**
+     * static
+     */
     public static void myStaticMethod() {
-
+        //static        : Instanz/Object-unabhängige Methode, Es muss kein Object instanziiert werden
     }
 
     public static void main(String[] args) {
@@ -40,9 +44,10 @@ public class Methods {
         myStaticMethod();
 
         Params myParams = Params.createParams();
-        myParams.a= 1;
-        myParams.b= 2;
+        myParams.a = 1;
+        myParams.b = 2;
 
         plus(myParams);
+
     }
 }
